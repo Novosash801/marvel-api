0618da502129b40cae8bcc86305a83d214125b20
Alexandr <chernousov013@gmail.com>
2024-04-06 11:51:28 +0700

    add components

---
+++ .eslintrc.cjs
@@ -0,0 +1,48 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    settings: { react: { version: '18.2' } },
+    plugins: ['react-refresh'],
+    rules: {
+        'react/jsx-no-target-blank': 'off',
+        'react-refresh/only-export-components': [
+
+            // My rules
+
+            'warn',
+                { allowConstantExport: true },
+            ],
+            'indent': [
+                'warn',
+                4
+            ],
+            'linebreak-style': [
+                'error',
+                'windows'
+            ],
+            'quotes': [
+                'error',
+                'single', {
+                    'allowTemplateLiterals': true
+                }
+            ],
+            'semi': [
+                'warn',
+                'always'
+            ],
+            'react/prop-types': [ 
+                'off'
+            ],
+            'no-unused-vars': [
+                'warn'
+            ],
+    },
+}
